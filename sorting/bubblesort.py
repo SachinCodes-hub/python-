2626bf8ef720877c2437d1c9cef0221742e4f1e1
sachincodes <sachinkarale2005@gmail.com>
2026-01-21 21:16:30 +0530

    desc

--- sorting/bubblesort.py
+++ sorting/bubblesort.py
@@ -1,5 +1,5 @@
 nums = [4,3,2,7,8,9]
-#Bubble sort - swap adjusunt element like comapere them and swap  . 
+#Bubble sort - swap adjasant element like compare them and swap  . 
 
 n = len(nums)
 for i in range(n-2 ,-1 , -1):
@@ -8,3 +8,14 @@ for i in range(n-2 ,-1 , -1):
             nums[j],nums[j+1] = nums[j+1],nums[j]
         
 print(nums)
+
+
+#Descending order
+arr = [6,4,2,1,8,5]
+
+for i in range(n-2, -1,-1):
+    for j in range(0 , i+1):
+        if arr[j+1] >arr[j]:
+            arr[j], arr[j+1] = arr[j+1],arr[j]
+        
+print(arr)
