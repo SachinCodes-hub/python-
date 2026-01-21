bec2964af5e0f9b5c790d3e4a790074c43af21da
sachincodes <sachinkarale2005@gmail.com>
2026-01-21 21:11:23 +0530

    buble sort

--- sorting/bubblesort.py
+++ sorting/bubblesort.py
@@ -1,2 +1,10 @@
 nums = [4,3,2,7,8,9]
 #Bubble sort - swap adjusunt element like comapere them and swap  . 
+
+n = len(nums)
+for i in range(n-2 ,-1 , -1):
+    for j in range(0 , i+1):
+        if nums[j+1] < nums[j]:
+            nums[j],nums[j+1] = nums[j+1],nums[j]
+        
+print(nums)
