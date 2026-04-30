b158def674cbfb6bc0c33aff0838e0583cd64466
sachincodes <sachinkarale2005@gmail.com>
2026-04-30 18:37:39 +0530

    Binarytree

---
+++ Binarytrees/Node.py
@@ -0,0 +1,41 @@
+#if we want to create binary tree first we need to create the nodes later we can connext them and create binary tree
+
+# Node in binary tree 
+
+class Node:
+    def __init__(self, val):
+        self.val = val 
+        self.left = None 
+        self.right = None
+
+
+#create nodes for each value
+
+drinks = Node("drinks")
+hot = Node("hot")
+cold = Node("cold")
+tea = Node("tea")
+coffee = Node("coffee")
+cola = Node("cola")
+fanta = Node("fanta")
+
+# connect them as your binary tree - reations
+hot.left = coffee
+hot.right = tea 
+cold.left = cola
+cold.right = fanta
+
+drinks.left = hot
+drinks.right = cold
+
+"""
+        drinks 
+        
+        hot.    cold'
+
+        tea coffee cola fanta
+"""
+
+# now our binary tree is created . 
+
+print(drinks.right.right.val) # fanta
