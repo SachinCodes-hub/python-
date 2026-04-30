8addef5d9bacc3fbf670c8faf0c833cf734279a3
sachincodes <sachinkarale2005@gmail.com>
2026-04-30 20:42:09 +0530

    maxheight

---
+++ Binarytrees/MAXheight.py
@@ -0,0 +1,4 @@
+# find the maximum depth of a binary tree [height]
+
+#depth node take the value from children node to parent node only the max value out of both and increment on the next above node .. 
+
