77e11430096b9fd04cc347eca58ac2b081677cb7
sachincodes <sachinkarale2005@gmail.com>
2026-05-02 14:51:29 +0530

    heaps

--- Heaps/minheap.py
+++ Heaps/minheap.py
@@ -13,7 +13,7 @@ class solution:
             self.arr[index] = new_val
             self.hepifydown(self.arr , index)
         
-    
+    # TC - O(log(N))
     def extractmin(self):
         if self.count == 0:
             return None
@@ -25,7 +25,29 @@ class solution:
             self.hepifydown(self.arr , 0 )
         return ele
     
+    
+    # TC -  o(1)
     def isEmpty(self):
         return self.count == 0 
     
+    def getmin(self):
+        return self.arr[0] if self.count > 0 else None
+    
+    def heapsize(self):
+        return self.count
+
+
+    def main():
+        heap = solution()
+        
+        heap.initializeHeap()
+        
+        heap.insert(4)
+        print("inserted 4")
+    
+        heap.insert(5)
+        print("inserted 5") 
+        
+        heap.insert(10)
+        print("inserted 10")       
     
