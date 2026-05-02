784324cf0da25ba62d9fbc2fa898f09830d49dbf
sachincodes <sachinkarale2005@gmail.com>
2026-05-02 15:20:52 +0530

    graph

--- Graphs/graph_representation.py
+++ Graphs/graph_representation.py
@@ -1,19 +1,23 @@
-n = 5 
-m = 6 
+n = 5 # number of nodes
+m = 6  # edges 
 
-edges = [[1,2],[2,4],[3,4],[1,3],[3,5],[5,4]]
+edges = [[1,2],[2,4],[3,4],[1,3],[3,5],[5,4]] # by this edges we know the connections between this nodes . 
 
 # 1 based index graph
 
 matrix = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
 print(matrix)
 
-
-for u , v in edges:
+#editing our edges into the 0 element matrix 
+for u,v in edges:
     matrix[u][v] = 1
     matrix[v][u] = 1
     
-    
+print(matrix) # output with the connections link value as 1 
+
+#[[0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 0, 0], [0, 1, 0, 0, 1, 0], [0, 1, 0, 0, 1, 1], [0, 0, 1, 1, 0, 1], [0, 0, 0, 1, 1, 0]]
+
+
 # List
 
 lst = [[] for _ in range(n+1)]
